fb0db749655139500c899a3325ab13c4d92a399f
머홍 <101238645+Wa-MeoHong@users.noreply.github.com>
2024-04-15 20:55:33 +0900

    Account(계좌)에 대한 서비스(create, get, delete) 제작

--- Backend/App/Service/Mydata_Service.py
+++ Backend/App/Service/Mydata_Service.py
@@ -10,15 +10,46 @@
 
     ========================================================================
 """
+from http import HTTPStatus
 
 from fastapi import HTTPException, status, File, UploadFile
-import csv
 import pandas as pd
+from passlib.context import CryptContext
+
+from App.database.tables import Account, Mydata
+from App.database.repository import AccountRepository, MydataRepository, UserRepository
+from App.Schema.Mydata_schema import AccountForm
+
 
 
 class MyDataService:
     def __init__(self):
         self.mydata = {}
+        self.account_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    def save_account_info(self, user_id: int, account: AccountForm, account_repo: AccountRepository):
+        account = Account.create(
+            userid=user_id,
+            accountnum=account.accountnum,
+            objective=account.Objective,
+        )
+        account_repo.save_account(account=account)
+        return account
+
+    def delete_account_info(self, account_id: int, account_repo: AccountRepository):
+        account_repo.delete_account(account_id=account_id)
+        return True
+
+    def get_account_info(self, user_id: int, account_num: int, account_repo: AccountRepository):
+        account_list = account_repo.get_account_by_user_id(user_id=user_id)
+        found_account = None
+        # 계좌번호가 일치하는지 확인 후 계좌를 찾음
+        for account in account_list:
+            db_account_num = account.accountnum
+            if db_account_num == account_num:
+                found_account = account
+                break
+        return found_account if found_account else None
 
     def get_mydata_from_csv(self, csvfile: UploadFile):
         df = pd.read_csv(csvfile.file)
