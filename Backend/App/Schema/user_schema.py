6eb7baee8c845ec8673a5b0b3fdfb740d6df3471
머홍 <101238645+Wa-MeoHong@users.noreply.github.com>
2024-04-15 20:43:34 +0900

    field validator 수정 완료

--- Backend/App/Schema/user_schema.py
+++ Backend/App/Schema/user_schema.py
@@ -9,13 +9,13 @@ class UserForm(BaseModel):
 
     # 유효성 검사를 진행하는 곳 (비어있는 칸이 있으면 확인)
     @field_validator('name', 'email')
-    def check_empty(self, value):
+    def check_empty(cls, value):
         if not value or value.isspace():
             raise HTTPException(status_code=422, detail="필수 항목을 입력 하세요.")
         return value
 
     @field_validator('password')
-    def check_password_empty(self, value):
+    def check_password_empty(cls, value):
         password = value.get_secret_value()
         if not password or password.isspace():
             raise HTTPException(status_code=422, detail="필수 항목을 입력 하세요.")
@@ -23,7 +23,7 @@ class UserForm(BaseModel):
 
     # 패스워드 8자리 이상 확인 및 영문 숫자 입력 확인
     @field_validator('password')
-    def check_password(self, value):
+    def check_password(cls, value):
         password = value.get_secret_value()
         if len(password) < 8:
             raise HTTPException(status_code=422, detail="비밀번호는 8자리 이상 영문과 숫자를 포함해 작성해 주세요.")
