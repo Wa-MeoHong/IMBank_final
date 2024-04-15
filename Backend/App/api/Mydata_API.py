11493ce8c4c181034163d77f1e76c53765453fa8
머홍 <101238645+Wa-MeoHong@users.noreply.github.com>
2024-04-15 20:56:19 +0900

    계좌 정보 관련 api 제작

--- Backend/App/api/Mydata_API.py
+++ Backend/App/api/Mydata_API.py
@@ -15,8 +15,10 @@ from typing_extensions import Annotated
 from App.Service.Mydata_Service import MyDataService
 from App.Service.user_Service import UserService
 from App.api.user_API import oauth2_scheme
-from App.database.repository import UserRepository
+from App.database.repository import UserRepository, AccountRepository, MydataRepository
+from App.Schema import Mydata_schema
 import pandas as pd
+
 
 app = APIRouter(
     prefix="/Mydata",
@@ -25,13 +27,83 @@ app = APIRouter(
 user_service = Annotated[UserService, Depends()]
 mydata_service = Annotated[MyDataService, Depends()]
 user_repository = Annotated[UserRepository, Depends()]
+account_repository = Annotated[AccountRepository, Depends()]
+mydata_repository = Annotated[MydataRepository, Depends()]
 
+@app.get("/getAccount")
+async def get_account(
+        mydata_serv: mydata_service,
+        user_serv: user_service,
+        user_repo: user_repository,
+        account_repo: account_repository,
+        account_num: int,
+        login_token: str = Depends(oauth2_scheme),
+
+):
+    try:
+        user_email = user_serv.decode_access_token(access_token=login_token)
+        if user_email is None:
+            raise
+    except Exception as e:
+        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not Login")
+    user_id = user_serv.get_user(email=user_email, user_repo=user_repo).id
+
+    account = mydata_serv.get_account_info(user_id=user_id, account_num=account_num, account_repo=account_repo)
+    return account if account is not None else HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+
+@app.post("/createAccount")
+async def create_account(
+        mydata_serv: mydata_service,
+        user_serv: user_service,
+        user_repo: user_repository,
+        account_repo: account_repository,
+        new_account: Mydata_schema.AccountForm = Depends(),
+        login_token: str = Depends(oauth2_scheme),
+):
+    try:
+        user_email = user_serv.decode_access_token(access_token=login_token)
+        if user_email is None:
+            raise
+    except Exception as e:
+        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not Login")
+    user_id = user_serv.get_user(email=user_email, user_repo=user_repo).id
+
+    account = mydata_serv.get_account_info(user_id=user_id, account_num=new_account.accountnum, account_repo=account_repo)
+    if account is not None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Account already exists")
+
+    account = mydata_serv.save_account_info(user_id=user_id, account=new_account, account_repo=account_repo)
+    return HTTPException(status_code=status.HTTP_201_CREATED, detail="Account created")
+
+@app.delete("/deleteAccount")
+async def delete_account(
+        mydata_serv: mydata_service,
+        user_serv: user_service,
+        user_repo: user_repository,
+        account_repo: account_repository,
+        account_num: int,
+        login_token: str = Depends(oauth2_scheme),
+):
+    try:
+        user_email = user_serv.decode_access_token(access_token=login_token)
+        if user_email is None:
+            raise
+    except Exception as e:
+        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not Login")
+    user_id = user_serv.get_user(email=user_email, user_repo=user_repo).id
+
+    account = mydata_serv.get_account_info(user_id=user_id, account_num=account_num, account_repo=account_repo)
+    if account is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="account Not Found")
+
+    mydata_serv.delete_account_info(account_id=account.id, account_repo=account_repo)
+    return HTTPException(status_code=status.HTTP_200_OK, detail="Account deleted")
 @app.post("/getMonthsPayData")
 async def get_months_Pay_data_from_csv(
         mydata_serv: mydata_service,
         user_serv: user_service,
         user_repo: user_repository,
-        # login_token: Annotated[oauth2_scheme, Depends(oauth2_scheme)],
+        login_token: Annotated[oauth2_scheme, Depends(oauth2_scheme)],
         csv_file: UploadFile = File(...),
 ):
     """
